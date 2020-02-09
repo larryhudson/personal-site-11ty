96843d5413d3d56c0389e2150fc7256041aab88e
Larry Hudson <larry@localhost.localdomain>
2020-02-10 06:50:59 +1100

    dark theme

---
+++ src/site/blog/2020/02/commit-lots-of-changes.md
@@ -0,0 +1,6 @@
+---
+title: git commit -m "lots of changes"
+date: 2020-02-10
+tags: ['post', 'web development', 'git']
+---
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'black': '#242125',
+        'purple': '#6500A1',
+        'blue': {
+          'dark': "#0a1945"
+        },
+        'brown': {
+          'text': '#341A04'
+        },
+        'light-blue': {
+          'bg': '#E8EEEA',
+          'tint': '#e0e6e2',
+        }
+      }
+    }
   },
   variants: {},
   plugins: []
